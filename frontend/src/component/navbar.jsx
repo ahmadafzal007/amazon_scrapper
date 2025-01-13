f3d522f3c8388e58a3322c877a6229c5e0c37df7
ahmadafzal007 <ahmadafzalch007@gmail.com>
2025-01-13 21:17:18 +0500

    navbar initiated

---
+++ frontend/src/component/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+
+
+
+    return (
+        <div className=" w-full h-16 mt-0 bg-[#1f2937] flex items-center justify-between">
+            <span className='font-serif font-extrabold text-white pl-2'>Codeaza</span>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ frontend/src/screens/main_screen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../component/navbar.jsx';
+
+
+const MainScreen = () => {
+    return (
+        <>
+        <Navbar/>
+        </>
+    );
+};
+
+export default MainScreen;
